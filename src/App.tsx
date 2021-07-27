d6a390f606318849c358da7fd99603cfab4abe8d
Samiul Haque Sohan <samiulhsohan@gmail.com>
2021-07-27 06:46:16 +0600

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
-
-const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+import { calculateWinner } from './utils';
 
 const App = () => {
   const [board, setBoard] = useState<string[]>(Array(9).fill(null));
@@ -28,17 +18,6 @@ const App = () => {
 
     setBoard(newBoard);
     setCrossTurn(!crossTurn);
-  };
-
-  const calculateWinner = (board: string[]) => {
-    for (let i = 0; i < winningCombinations.length; i++) {
-      const [a, b, c] = winningCombinations[i];
-
-      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        return board[a];
-      }
-    }
-    return null;
   };
 
   useEffect(() => {
---
+++ src/utils/winner.ts
@@ -0,0 +1,23 @@
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const calculateWinner = (board: string[]) => {
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+  return null;
+};
+
+export default calculateWinner;
