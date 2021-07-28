af81d5fd1e1e162080194691233f1deded04614f
Samiul Haque Sohan <samiulhsohan@gmail.com>
2021-07-28 15:13:20 +0600

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { useAppSelector, useAppDispatch } from './store/hooks';
-import { getBoard, getWinner, restartGame } from './store/game';
+import { getWinner, restartGame } from './store/game';
 import Board from './components/Board';
 import GameInfo from './components/GameInfo';
 import GameControl from './components/GameControl';
@@ -11,7 +11,6 @@ import GameControl from './components/GameControl';
 const App = () => {
   const dispatch = useAppDispatch();
 
-  const board = useAppSelector(getBoard);
   const winner = useAppSelector(getWinner);
 
   const [loading, setLoading] = useState(true);
@@ -19,7 +18,7 @@ const App = () => {
   // using ref to ignore useEffect's dependency linting warning
   const initiate = useRef(() => {});
   initiate.current = () => {
-    if (winner || board.every((square) => square !== null)) {
+    if (winner) {
       dispatch(restartGame());
     }
     setLoading(false);
