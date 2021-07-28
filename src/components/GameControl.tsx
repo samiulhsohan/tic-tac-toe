619600cb7f35c47d8895682535aca9ac65364aa8
Samiul Haque Sohan <samiulhsohan@gmail.com>
2021-07-28 12:35:38 +0600

    refactor game control

--- src/components/GameControl.tsx
+++ src/components/GameControl.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { resetGame, restartGame } from '../store/game';
 import { useAppDispatch } from '../store/hooks';
+import Button from './Button';
 
 const GameControl = () => {
   const dispatch = useAppDispatch();
 
   return (
     <div className="game-control">
-      <button onClick={() => dispatch(restartGame())}>Restart Game</button>
-      <button onClick={() => dispatch(resetGame())}>Reset Game</button>
+      <Button onClick={() => dispatch(restartGame())}>Restart Game</Button>
+      <Button onClick={() => dispatch(resetGame())}>Reset Game</Button>
     </div>
   );
 };
